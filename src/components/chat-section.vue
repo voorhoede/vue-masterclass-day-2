ad6a8ff1c3b49884ee1d7b0627c6a9db6167c95e
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-05-31 14:46:24 +0200

    add scroll behaviour for when new message is added

--- src/components/chat-section.vue
+++ src/components/chat-section.vue
@@ -33,6 +33,14 @@ export default {
   computed: {
     ...mapGetters(['currentUser'])
   },
+  watch: {
+    messages() {
+      this.$nextTick(() => {
+        const messagesEl = this.$refs.messages
+        messagesEl.scrollTop = messagesEl.scrollHeight
+      })
+    }
+  },
   methods: {
     ...mapActions(['sendMessage']),
     createMessage() {
