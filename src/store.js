1df99b4aadece38b5821716d12ccb668bd0d9aaf
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-06-13 14:03:15 +0200

    feat(store): add actions for messages and users

--- src/store.js
+++ src/store.js
@@ -9,21 +9,23 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     users: [],
-    user: {
-      name: "Anoniempje",
-      avatar: "./images/avatar.png"
-    },
+    user: api.getUserId(),
     messages: [],
     searching: false,
     searchText: ''
   },
   getters: {
     messages (state) {
-      return state.messages.map(message => {
-        const user = state.users.find(user => user._id === message.user)
-        message.user = user
-        return message
-      })
+      return state.messages
+        .filter(message => message.user)
+        .map(message => {
+          message.user = state.users.find(user => user._id === (message.user._id || message.user))
+          return message
+        })
+        .sort((a, b) => a.date - b.date)
+    },
+    currentUser(state) {
+      return state.users.find(user => user._id === state.user);
     }
   },
   mutations: {
@@ -36,8 +38,17 @@ export default new Vuex.Store({
     setUsers(state, users) {
       state.users = users
     },
-    setUserName(state, name) {
-      state.user.name = name
+    addUser(state, user) {
+      state.users.push(user)
+    },
+    updateUser(state, userData) {
+      state.users = state.users.map(user => {
+        if (user._id === userData._id) {
+          return { ...user, ...userData }
+        } else {
+          return user
+        }
+      })
     },
     setSearching(state, status) {
       state.searching = status
@@ -47,11 +58,27 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    async updateUsername ({ getters, commit }, username) {
+      const updatedUser = { ...getters.currentUser, name: username }
+      const user = await api.updateUser(updatedUser)
+      commit('updateUser', user)
+    },
+    async sendMessage(store, message) {
+      api.addMessage(message)
+    },
     async initChat({ commit }) {
       const { messages, users } = await api.init()
 
       commit('setUsers', users)
       commit('setMessages', messages)
+
+      api.startStream(doc => {
+        if (doc.type === 'message') {
+          commit('addMessage', doc)
+        } else {
+          commit('addUser', doc)
+        }
+      });
     }
   }
 })
