05bc4c8743696207fe4336066f45bf9aab0c43ce
Remco <remco@voorhoede.nl>
2019-07-17 17:21:11 +0200

    feat: add solution

--- src/components/message-field.spec.js
+++ src/components/message-field.spec.js
@@ -1,13 +1,38 @@
 import { mount } from '@vue/test-utils';
+import MessageField from './message-field.vue';
 
 it('emits the message when pressing enter', () => {
+  const wrapper = mount(MessageField);
 
+  // enter the text into the input field and trigger a input event
+  wrapper.find('input[type=text]').setValue('Some value');
+
+  // trigger the form submit
+  wrapper.find('form').trigger('submit');
+
+  expect(wrapper.emitted().submit).toEqual([["Some value"]]);
 });
 
 it('allows you to add emojis', () => {
+  const wrapper = mount(MessageField);
 
+  // find the emoji that i want to click (not the nicest code)
+  const emoji = wrapper.findAll('button').filter(i => i.text().match('😄'));
+
+  // click it!
+  emoji.trigger('click');
+
+  expect(wrapper.find('input[type=text]').element.value).toBe('😄');
 });
 
 it('clears the text after it has been submitted', () => {
+  const wrapper = mount(MessageField);
 
+  // enter the text into the input field and trigger a input event
+  wrapper.find('input[type=text]').setValue('Some value');
+
+  // trigger the form submit
+  wrapper.find('form').trigger('submit');
+
+  expect(wrapper.find('input[type=text]').element.value).toEqual('');
 });
