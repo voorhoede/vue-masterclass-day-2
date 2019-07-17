e12d131b7c1c23f4f5d9911bbcefe529532643b1
Remco <remco@voorhoede.nl>
2019-07-17 16:36:43 +0200

    feat: add jest setup

---
+++ src/components/message-field.spec.js
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils';
+
+it('emits the message when pressing enter', () => {
+
+});
+
+it('allows you to add emojis', () => {
+
+});
+
+it('clears the text after it has been submitted', () => {
+
+});
