9ab30a29cef696c8dad734f3f494e6cefb8110fd
Remco <remco@voorhoede.nl>
2019-07-20 14:51:07 +0200

    implement error

--- src/components/chat-section.vue
+++ src/components/chat-section.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="chat-section">
-    <!-- show the message below when an error has occured within the application -->
-    <!-- <p class="error">Sorry, an error occured. Please try again later!</p> -->
+    <p v-if="error" class="error">Sorry, an error occured. Please try again later!</p>
     <div ref="messages" class="chat-section__messages">
       <message-list :messages="messages"/>
     </div>
@@ -37,6 +36,10 @@ export default {
         messagesEl.scrollTop = messagesEl.scrollHeight
       })
     }
+  },
+  errorCaptured(error) {
+    this.error = true;
+    return false;
   },
   computed: {
     ...mapGetters(['currentUser'])
