ec96811f644be0280d18d48ab935cf7856933857
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2020-04-21 13:52:06 +0200

    add SFC exercise solution

---
+++ src/components/search-form.vue
@@ -0,0 +1,40 @@
+<template>
+  <form class="search-form" @submit.prevent="onSubmit">
+    <input type="search" v-model="searchText" placeholder="Search" class="search-field">
+  </form>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return window.chat;
+  },
+  methods: {
+    onSubmit() {
+      this.searching = true;
+    }
+  }
+};
+</script>
+
+<style>
+.search-form {
+  margin-left: auto;
+  margin-bottom: 0;
+  display: flex;
+  width: 350px;
+  margin-right: calc(-1 * var(--spacing));
+}
+
+.search-field {
+  -webkit-appearance: none;
+  outline: none;
+  font: inherit;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 0.3em 0.4em 0.3em 2em;
+  background: url("../assets/search.svg") no-repeat 5px center;
+  line-height: 34px;
+  height: 34px;
+}
+</style>
